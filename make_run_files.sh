ee712c31b1f7bfe834053a2309f6b0fac00e531c
LuciMoore <lucille.a.mo@gmail.com>
2023-10-29 18:49:00 -0500

    updated template file to correctly feed arguments into run_crossotope_LR script and function

--- make_run_files.sh
+++ make_run_files.sh
@@ -4,15 +4,15 @@ set +x
 
 # Get bin for minutes of rest to sample from the config.json
 json_file="config.json"
-max_minutes=$(grep -o '"max_minutes": *[0-9]*' "$json_file" | awk -F': ' '{print $2}')min
+min=$(grep -o '"max_minutes": *[0-9]*' "$json_file" | awk -F': ' '{print $2}')min
 
 # determine data directory, run folders, and run templates
 run_folder=`pwd`
 
-work_dir="/tmp"
+work_dir="${run_folder}/tier1_wd"
 data_dir="/home/faird/shared/projects/MSC_to_DCAN/split_halves/half1"
 
-out_dir="${run_folder}/CORR_OUT/${max_minutes}"
+out_dir="${run_folder}/CORR_OUT/${min}"
 half2_ILI="${run_folder}/half2_ILI"
 reliability_folder="${run_folder}/run_files.reliability_full"
 reliability_template="template.reliability_full_run"
@@ -36,7 +36,7 @@ fi
 
 # make output directory if missing
 if [ ! -d "${out_dir}" ]; then
-	mkdir "${out_dir}"
+	mkdir -p "${out_dir}"
 fi
 
 # counter to create run numbers
@@ -45,7 +45,7 @@ k=0
 subjects=(MSC01 MSC02 MSC03 MSC04 MSC05 MSC06 MSC07 MSC08 MSC09 MSC10)
 
 for subject in "${subjects[@]}"; do
-	sed -e "s|SUBJECTID|${subject}|g" -e "s|WORKDIR|${work_dir}|g" -e "s|DATADIR|${data_dir}|g" -e "s|RUNDIR|${run_folder}|g" -e "s|OUTDIR|${out_dir}|g" -e "s|HALF2ILI|${half2_ILI}|g" ${run_folder}/${reliability_template} > ${reliability_folder}/run${k}
+	sed -e "s|SUBJECTID|${subject}|g" -e "s|WORKDIR|${work_dir}|g" -e "s|DATADIR|${data_dir}|g" -e "s|RUNDIR|${run_folder}|g" -e "s|OUTDIR|${out_dir}|g" -e "s|HALF2ILI|${half2_ILI}|g" -e "s|MIN|${min}|g" ${run_folder}/${reliability_template} > ${reliability_folder}/run${k}
 	k=$((k+1))
 done
 
--- resources_reliability_full_run.sh
+++ resources_reliability_full_run.sh
@@ -2,20 +2,21 @@
 
 #SBATCH -J MSC
 #SBATCH --nodes=1
-#SBATCH --ntasks-per-node=1
-#SBATCH --mem-per-cpu=32gb
-#SBATCH --time=15:00:00
+#SBATCH --ntasks-per-node=7
+#SBATCH --mem-per-cpu=8gb
+#SBATCH --time=10:00:00
 #SBATCH -p amdsmall,amdlarge,amd512,small,ram256g,msismall
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=lmoore@umn.edu
 #SBATCH -o output_logs/reliability_%A_%a.out
 #SBATCH -e output_logs/reliability_%A_%a.err
-#SBATCH -A elisonj
+#SBATCH -A rando149
 
 cd run_files.reliability_full
 
 module purge
 module load python3 matlab/R2019a
+module load parallel
 
 export PATH="/home/faird/shared/code/external/utilities/workbench/1.4.2/workbench/bin_rh_linux64/:${PATH}"
 which wb_command
